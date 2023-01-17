7f7442da60f8e09c1ca95de78ea749e17bf3d3c1
Daniel Nikolov <danielsnikolov@gmail.com>
2023-01-17 12:09:49 +0200

    Added default init to collection in Tournament

--- TournamentsRegister/Models/Tournament.cs
+++ TournamentsRegister/Models/Tournament.cs
@@ -4,7 +4,9 @@ namespace TournamentsRegister.Models;
 
 public class Tournament : ReadOnlyPrimaryKeyUser
 {
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
     public string Name { get; set; }
     public string Description { get; set; }
-    public List<Team> Teams { get; set; }
+    public List<Team> Teams { get; set; } = new();
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 }
