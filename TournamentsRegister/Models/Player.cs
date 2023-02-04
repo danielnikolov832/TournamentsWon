700f47c9e67364055115b6d4b617ee1f1bb6e936
Daniel Nikolov <danielsnikolov@gmail.com>
2023-02-04 09:28:43 +0200

    Made name not nullable

--- TournamentsRegister/Models/Player.cs
+++ TournamentsRegister/Models/Player.cs
@@ -4,5 +4,7 @@ namespace TournamentsRegister.Models;
 
 public class Player : ReadOnlyPrimaryKeyUser
 {
-    public string? Name { get; set; }
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    public string Name { get; set; }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 }
